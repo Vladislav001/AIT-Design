77436f550d0fc2d5da01e466d241240b3bd67c6c
Vladislav <VladGurjev@mail.ru>
2017-06-24 20:08:40 +0400

    подкрутил регистрацию(пока не все фичи)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 module.exports = function(app) {
   // Каждый 'get' подключает соотсветсвующий модуль и вызывает его метод 'get'
   app.get('/', require('./login').get);
-  //app.post('/login', require('./login').post); // при poste на login, подключаем post этого модуля()
+  app.post('/login', require('./login').post); // при poste на login, подключаем post этого модуля()
   app.get('/personalArea', require('./personalArea').get);
   app.get('/resultTest', require('./resultTest').get);
   app.get('/testSettings', require('./testSettings').get);
