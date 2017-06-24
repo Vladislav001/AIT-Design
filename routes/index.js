646d3a031afdb45bf894ba59607043dfc933dd18
Vladislav <VladGurjev@mail.ru>
2017-06-24 18:52:16 +0400

    Немного перенес на node js

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+//var checkAuth = require('../middleware/checkAuth');
+
+module.exports = function(app) {
+  // Каждый 'get' подключает соотсветсвующий модуль и вызывает его метод 'get'
+  app.get('/', require('./login').get);
+  //app.post('/login', require('./login').post); // при poste на login, подключаем post этого модуля()
+  app.get('/personalArea', require('./personalArea').get);
+  app.get('/resultTest', require('./resultTest').get);
+    app.get('/testSettings', require('./testSettings').get);
+};
