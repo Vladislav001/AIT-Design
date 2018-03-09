987428d330c4503e31c9b83f5c6548c2b6d5f33d
Vladislav <VladGurjev@mail.ru>
2018-03-09 15:31:48 +0300

    Регулировка размера range(пока через js)

---
+++ public/js/countImagesRange.js
@@ -0,0 +1,6 @@
+// Присвоим кол-во значений в range
+$("#styleImagesSwap").attr({max:3});
+$("#styleImagesLikeDislike").attr({max:3});
+$("#styleImageStopTest").attr({max:3});
+$("#styleImageResult").attr({max:3});
+$("#styleImageFinish").attr({max:3});
